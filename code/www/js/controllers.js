50f39facf157ecb9e4d4233a2dcc3f035c264a30
cstallings1 <catiestallings1@gmail.com>
2016-03-29 21:45:15 -0500

    Add favorites functionality

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -4,7 +4,7 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope, $timeout) {
+.controller('DiscoverCtrl', function($scope, $timeout, User) {
   $scope.songs = [
     {
         "title":"Stealing Cinderella",
@@ -28,9 +28,10 @@ Controller for the discover page
   $scope.currentSong = angular.copy($scope.songs[0])
 
   $scope.sendFeedback = function (bool) {
-
+    if (bool) User.addSongToFavorites($scope.currentSong);
     $scope.currentSong.rated = bool;
     $scope.currentSong.hide = true;
+
 
     $timeout(function() {
       var randomSong = Math.round(Math.random() * ($scope.songs.length - 1));
@@ -43,8 +44,8 @@ Controller for the discover page
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope) {
-
+.controller('FavoritesCtrl', function($scope, User) {
+  $scope.favorites = User.favorites;
 })
 
 
