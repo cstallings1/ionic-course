320593bc1ec0a903b7aa540947debf1f76433b43
cstallings1 <catiestallings1@gmail.com>
2016-05-01 14:33:16 -0500

    Add favorite count  badge

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -58,10 +58,14 @@ Controller for the discover page
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope, User) {
+.controller('FavoritesCtrl', function($scope, $window, User) {
   $scope.favorites = User.favorites;
   $scope.removeSong = function(song, index) {
     User.removeSongFromFavorites(song, index);
+  }
+
+  $scope.openSong = function(song) {
+    $window.open(song.open_url, "_system");
   }
 })
 
@@ -69,12 +73,16 @@ Controller for the favorites page
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope, Recommendations) {
+.controller('TabsCtrl', function($scope, User, Recommendations) {
+  $scope.favCount = User.favoriteCount;
+
   $scope.enteringFavorites = function() {
+    User.newFavorites = 0;
     Recommendations.haltAudio();
   }
 
   $scope.leavingFavorites = function() {
     Recommendations.init();
   }
+
 });
